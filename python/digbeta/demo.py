77c0c0477fe7d7fd7175d74ca1a666c25b25f1c2
Cheng Soon Ong <chengsoon.ong@unimelb.edu.au>
2015-05-20 18:14:31 +1000

    Implemented CPUCB. Minor refactoring

--- python/digbeta/demo.py
+++ python/digbeta/demo.py
@@ -3,7 +3,7 @@
 import numpy as np
 from environment import MAB
 from arm import Bernoulli
-from policy import UCB, klUCB
+from policy import UCB, klUCB, CPUCB
 from experiment import Evaluation
 
 def scenario0(num_rep=10, horizon=2000):
@@ -11,13 +11,16 @@ def scenario0(num_rep=10, horizon=2000):
     Figure 2 in Garivier & Cappe, COLT 2011"""
     probs = [0.1, 0.05, 0.05, 0.05, 0.02, 0.02, 0.02, 0.01, 0.01, 0.01]
     world = MAB([Bernoulli(p) for p in probs])
-    policies = [UCB(world.num_arms), klUCB(world.num_arms)]
+    policies = [CPUCB(world.num_arms), UCB(world.num_arms), klUCB(world.num_arms)]
 
     t_save = np.linspace(100, horizon-1, 200, dtype=int)
     for pol in policies:
+        print('Evaluation of %s' % pol.name)
         w = Evaluation(world, pol, num_rep, horizon, t_save)
-        print(w.mean_reward())
+        print('Mean reward = %1.2f' % w.mean_reward())
+        print('Mean number of draws for each of %d repetitions' % num_rep)
         print(w.mean_num_draws())
+        print('Mean regret')
         mean_regret = w.mean_regret()
         print(mean_regret)
 
--- python/digbeta/policy.py
+++ python/digbeta/policy.py
@@ -2,22 +2,34 @@
 
 import random
 from math import log
+import scipy.stats
 import kullback
 
 class Policy:
     """Class that implements a generic index policy class."""
 
-    def __init__(self):
-        pass
+    def __init__(self, num_arms, amplitude=1., lower=0.):
+        self.num_arms = num_arms
+        self.num_draws = dict()
+        self.cum_reward = dict()
+        self.t = 0
+        self.amplitude = amplitude
+        self.lower = lower
 
-    def start_game(self, num_arms):
-        raise NotImplementedError
+    @property
+    def name(self):
+        return self.__class__.__name__
+    
+    def start_game(self):
+        self.t = 1
+        for arm in range(self.num_arms):
+            self.num_draws[arm] = 0
+            self.cum_reward[arm] = 0.0
 
-    def get_reward(self):
-        raise NotImplementedError
-
-    def compute_index(self, arm):
-        raise NotImplementedError
+    def get_reward(self, arm, reward):
+        self.num_draws[arm] += 1
+        self.cum_reward[arm] += (reward - self.lower) / self.amplitude
+        self.t += 1
 
     def choice(self):
         """In an index policy, choose at random an arm with maximal index."""
@@ -28,23 +40,16 @@ class Policy:
         best_arms = [arm for arm in index.keys() if index[arm] == max_index]
         return random.choice(best_arms)
 
+    def compute_index(self, arm):
+        raise NotImplementedError
+
 class klUCB(Policy):
     """The generic kl-UCB policy for one-parameter exponential distributions.
       """
-    def __init__(self, num_arms, amplitude=1., lower=0., klucb=kullback.klucbBern):
+    def __init__(self, num_arms, klucb=kullback.klucbBern):
+        Policy.__init__(self, num_arms)
         self.c = 1.
-        self.num_arms = num_arms
-        self.amplitude = amplitude
-        self.lower = lower
-        self.num_draws = dict()
-        self.cum_reward = dict()
         self.klucb = klucb
-
-    def start_game(self):
-        self.t = 1
-        for arm in range(self.num_arms):
-            self.num_draws[arm] = 0
-            self.cum_reward[arm] = 0.0
 
     def compute_index(self, arm):
         if self.num_draws[arm] == 0:
@@ -53,14 +58,6 @@ class klUCB(Policy):
             # Could adapt tolerance to the value of self.t
             return self.klucb(self.cum_reward[arm] / self.num_draws[arm],
                               self.c * log(self.t) / self.num_draws[arm], 1e-4)
-
-    def get_reward(self, arm, reward):
-        self.num_draws[arm] += 1
-        self.cum_reward[arm] += (reward - self.lower) / self.amplitude
-        self.t += 1
-    # Debugging code
-    #print "arm " + str(arm) + " receives " + str(reward)
-    #print str(self.nbDraws[arm]) + " " + str(self.cumReward[arm])
 
 class UCB(klUCB):
     """The Upper Confidence Bound (UCB) index.
@@ -72,5 +69,33 @@ class UCB(klUCB):
     Note that UCB is implemented as a special case of klUCB for the divergence 
     corresponding to the Gaussian distributions, see [Garivier & Cappé - COLT, 2011].
     """
-    def __init__(self, nbArms, amplitude=1., lower=0.):
-        klUCB.__init__(self, nbArms, amplitude, lower, lambda x, d, sig2: kullback.klucbGauss(x, d, .25))
+    def __init__(self, num_arms):
+        klUCB.__init__(self, num_arms, lambda x, d, sig2: kullback.klucbGauss(x, d, .25))
+
+
+def clopper_pearson(k, n, alpha=0.95):
+    """
+    http://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval
+    alpha confidence intervals for a binomial distribution of k expected successes on n trials
+    Clopper Pearson intervals are a conservative estimate.
+    """
+    lo = scipy.stats.beta.ppf(alpha/2, k, n-k+1)
+    hi = scipy.stats.beta.ppf(1 - alpha/2, k+1, n-k)
+    return lo, hi
+
+class CPUCB(Policy):
+    """Clopper-Pearson UCB
+    [Garivier & Cappé, COLT 2011]
+    """
+    def __init__(self, num_arms, c=1.01):
+        """c is the parameter of the UCB"""
+        Policy.__init__(self, num_arms)
+        self.c = c
+        
+    def compute_index(self, arm):
+        if self.num_draws[arm] == 0:
+            return float('+infinity')
+        else:
+            lcb, ucb = clopper_pearson(self.cum_reward[arm], self.num_draws[arm], 1./(self.t**self.c))
+            return ucb
+
