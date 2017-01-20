476021f9aec023b3d65735a92a9016dac1a53834
Dawei Chen <dawei.chen@data61.csiro.au>
2017-01-20 16:34:35 +1100

    Python module for inference procedures (issue #109, #113)

---
+++ ssvm/src/inference.py
@@ -0,0 +1,215 @@
+import numpy as np
+import pandas as pd
+import itertools
+import pulp
+
+
+def do_inference_brute_force(ps, L, M, unary_params, pw_params, unary_features, pw_features, 
+                             y_true=None, y_true_list=None, debug=False):
+    """
+    Inference using brute force search (for sanity check), could be:
+    - Train/prediction inference for single-label SSVM
+    - Train/prediction inference for multi-label SSVM
+    """
+    assert(L > 1)
+    assert(L <= M)
+    assert(ps >= 0)
+    assert(ps < M)
+    if y_true is not None: assert(y_true_list is not None and type(y_true_list) == list)
+    
+    Cu = np.zeros(M, dtype=np.float)      # unary_param[p] x unary_features[p]
+    Cp = np.zeros((M, M), dtype=np.float) # pw_param[pi, pj] x pw_features[pi, pj]
+    # a intermediate POI should NOT be the start POI, NO self-loops
+    for pi in range(M):
+        Cu[pi] = np.dot(unary_params[pi, :], unary_features[pi, :]) # if pi != ps else -np.inf
+        for pj in range(M):
+            Cp[pi, pj] = -np.inf if (pj == ps or pi == pj) else np.dot(pw_params[pi, pj, :], pw_features[pi, pj, :])
+    
+    max_score = 0
+    y_best = None
+    for x in itertools.permutations([p for p in range(M) if p != ps], int(L-1)):
+        y = [ps] + list(x)
+        score = 0
+        
+        if y_true is not None and np.any([np.all(np.array(y) == np.asarray(yj)) for yj in y_true_list]) == True: continue
+        
+        for j in range(1, L): score += Cp[y[j-1], y[j]] + Cu[y[j]]
+        if y_true is not None: score += np.sum(np.asarray(y) != np.asarray(y_true))
+        
+        if score > max_score:
+            max_score = score
+            y_best = y
+    if debug == True: print(max_score)
+    return y_best
+
+
+
+def do_inference_greedy(ps, L, M, unary_params, pw_params, unary_features, pw_features, y_true=None, y_true_list=None):
+    """
+    Inference using greedy search (baseline), could be:
+    - Train/prediction inference for single-label SSVM
+    - Prediction inference for multi-label SSVM
+    """
+    assert(L > 1)
+    assert(L <= M)
+    assert(ps >= 0)
+    assert(ps < M)
+    if y_true is not None:
+        assert(y_true_list is not None and type(y_true_list) == list)
+        assert(len(y_true_list) == 1)
+    
+    Cu = np.zeros(M, dtype=np.float)      # unary_param[p] x unary_features[p]
+    Cp = np.zeros((M, M), dtype=np.float) # pw_param[pi, pj] x pw_features[pi, pj]
+    # a intermediate POI should NOT be the start POI, NO self-loops
+    for pi in range(M):
+        Cu[pi] = np.dot(unary_params[pi, :], unary_features[pi, :]) # if pi != ps else -np.inf
+        for pj in range(M):
+            Cp[pi, pj] = -np.inf if (pj == ps or pi == pj) else np.dot(pw_params[pi, pj, :], pw_features[pi, pj, :])
+    
+    y_hat = [ps]
+    for t in range(1, L):
+        candidate_points = [p for p in range(M) if p not in y_hat]
+        p = y_hat[-1]
+        maxix = np.argmax([Cp[p, p1] + Cu[p1] + float(p1 != y_true[t]) if y_true is not None else \
+                           Cp[p, p1] + Cu[p1] for p1 in candidate_points])
+        y_hat.append(candidate_points[maxix])
+        
+    return np.asarray(y_hat)
+
+
+
+def do_inference_viterbi(ps, L, M, unary_params, pw_params, unary_features, pw_features, y_true=None, y_true_list=None):
+    """
+    Inference using the Viterbi algorithm, could be:
+    - Train/prediction inference for single-label SSVM
+    - Prediction inference for multi-label SSVM
+    """
+    assert(L > 1)
+    assert(L <= M)
+    assert(ps >= 0)
+    assert(ps < M)
+    if y_true is not None:
+        assert(y_true_list is not None and type(y_true_list) == list)
+        assert(len(y_true_list) == 1)
+    
+    Cu = np.zeros(M, dtype=np.float)      # unary_param[p] x unary_features[p]
+    Cp = np.zeros((M, M), dtype=np.float) # pw_param[pi, pj] x pw_features[pi, pj]
+    # a intermediate POI should NOT be the start POI, NO self-loops
+    for pi in range(M):
+        Cu[pi] = np.dot(unary_params[pi, :], unary_features[pi, :]) # if pi != ps else -np.inf
+        for pj in range(M):
+            Cp[pi, pj] = -np.inf if (pj == ps or pi == pj) else np.dot(pw_params[pi, pj, :], pw_features[pi, pj, :])
+    
+    A = np.zeros((L-1, M), dtype=np.float)     # scores matrix
+    B = np.ones((L-1, M), dtype=np.int) * (-1) # backtracking pointers
+    
+    for p in range(M): # ps--p
+        A[0, p] = Cp[ps, p] + Cu[p]
+        #if y_true is not None and p != ps: A[0, p] += float(p != y_true[1])/L  # loss term: normalised
+        if y_true is not None and p != ps: A[0, p] += float(p != y_true[1])
+        B[0, p] = ps
+
+    for t in range(0, L-2):
+        for p in range(M):
+            #loss = float(p != y_true[l+2])/L if y_true is not None else 0  # loss term: normlised
+            loss = float(p != y_true[t+2]) if y_true is not None else 0
+            scores = [A[t, p1] + Cp[p1, p] + Cu[p] for p1 in range(M)] # ps~~p1--p
+            maxix = np.argmax(scores)
+            A[t+1, p] = scores[maxix] + loss
+            #B[l+1, p] = np.array(range(N))[maxix]
+            B[t+1, p] = maxix
+
+    y_hat = [np.argmax(A[L-2, :])]
+    p, t = y_hat[-1], L-2
+    while t >= 0:
+        y_hat.append(B[t, p])
+        p, t = y_hat[-1], t-1
+    y_hat.reverse()
+
+    return np.asarray(y_hat)
+
+
+
+def do_inference_ILP(ps, L, M, unary_params, pw_params, unary_features, pw_features, y_true=None, y_true_list=None):
+    """
+    Inference using integer linear programming (ILP), could be:
+    - Train/prediction inference for single-label SSVM (NOTE: NOT Hamming loss)
+    - Prediction inference for multi-label SSVM
+    """
+    assert(L > 1)
+    assert(L <= M)
+    assert(ps >= 0)
+    assert(ps < M)
+    if y_true is not None:
+        assert(y_true_list is not None and type(y_true_list) == list)
+        assert(len(y_true_list) == 1)
+
+    p0 = str(ps)
+    pois = [str(p) for p in range(M)] # create a string list for each POI
+    pb = pulp.LpProblem('Inference_ILP', pulp.LpMaximize) # create problem
+    # visit_i_j = 1 means POI i and j are visited in sequence
+    visit_vars = pulp.LpVariable.dicts('visit', (pois, pois), 0, 1, pulp.LpInteger) 
+    # isend_l = 1 means POI l is the END POI of trajectory
+    isend_vars = pulp.LpVariable.dicts('isend', pois, 0, 1, pulp.LpInteger) 
+    # a dictionary contains all dummy variables
+    dummy_vars = pulp.LpVariable.dicts('u', [x for x in pois if x != p0], 2, M, pulp.LpInteger)
+    
+    # add objective
+    objlist = []
+    for pi in pois:     # from
+        for pj in pois: # to
+            objlist.append(visit_vars[pi][pj] * (np.dot(unary_params[int(pj)], unary_features[int(pj)]) + \
+                                                 np.dot(pw_params[int(pi), int(pj)], pw_features[int(pi), int(pj)])))
+    if y_true is not None: # Loss: normalised number of mispredicted POIs, Hamming loss is non-linear of 'visit'
+        objlist.append(1)
+        for j in range(M):
+            pj = pois[j]
+            for k in range(1, L): 
+                pk = str(y_true[k])
+                #objlist.append(-1.0 * visit_vars[pj][pk] / L) # loss term: normalised
+                objlist.append(-1.0 * visit_vars[pj][pk])
+    pb += pulp.lpSum(objlist), 'Objective'
+    
+    # add constraints, each constraint should be in ONE line
+    pb += pulp.lpSum([visit_vars[pi][pi] for pi in pois]) == 0, 'NoSelfLoops'
+    pb += pulp.lpSum([visit_vars[p0][pj] for pj in pois]) == 1, 'StartAt_p0'
+    pb += pulp.lpSum([visit_vars[pi][p0] for pi in pois]) == 0, 'NoIncoming_p0'
+    pb += pulp.lpSum([visit_vars[pi][pj] for pi in pois for pj in pois]) == L-1, 'Length'
+    pb += pulp.lpSum([isend_vars[pi] for pi in pois]) == 1, 'OneEnd'
+    pb += isend_vars[p0] == 0, 'StartNotEnd'
+    
+    for pk in [x for x in pois if x != p0]:
+        pb += pulp.lpSum([visit_vars[pi][pk] for pi in pois]) == isend_vars[pk] + \
+              pulp.lpSum([visit_vars[pk][pj] for pj in pois if pj != p0]), 'ConnectedAt_' + pk
+        pb += pulp.lpSum([visit_vars[pi][pk] for pi in pois]) <= 1, 'Enter_' + pk + '_AtMostOnce'
+        pb += pulp.lpSum([visit_vars[pk][pj] for pj in pois if pj != p0]) + isend_vars[pk] <= 1, \
+              'Leave_' + pk + '_AtMostOnce'
+    for pi in [x for x in pois if x != p0]:
+        for pj in [y for y in pois if y != p0]:
+            pb += dummy_vars[pi] - dummy_vars[pj] + 1 <= (M - 1) * (1 - visit_vars[pi][pj]), \
+                  'SubTourElimination_' + pi + '_' + pj
+    #pb.writeLP("traj_tmp.lp")
+    
+    # solve problem: solver should be available in PATH
+    if USE_GUROBI == True:
+        gurobi_options = [('TimeLimit', '7200'), ('Threads', str(N_JOBS)), ('NodefileStart', '0.2'), ('Cuts', '2')]
+        pb.solve(pulp.GUROBI_CMD(path='gurobi_cl', options=gurobi_options)) # GUROBI
+    else:
+        pb.solve(pulp.COIN_CMD(path='cbc', options=['-threads', str(N_JOBS), '-strategy', '1', '-maxIt', '2000000']))#CBC
+    visit_mat = pd.DataFrame(data=np.zeros((len(pois), len(pois)), dtype=np.float), index=pois, columns=pois)
+    isend_vec = pd.Series(data=np.zeros(len(pois), dtype=np.float), index=pois)
+    for pi in pois:
+        isend_vec.loc[pi] = isend_vars[pi].varValue
+        for pj in pois: visit_mat.loc[pi, pj] = visit_vars[pi][pj].varValue
+
+    # build the recommended trajectory
+    recseq = [p0]
+    while True:
+        pi = recseq[-1]
+        pj = visit_mat.loc[pi].idxmax()
+        value = visit_mat.loc[pi, pj]
+        assert(int(round(value)) == 1)
+        recseq.append(pj)
+        if len(recseq) == L: 
+            assert(int(round(isend_vec[pj])) == 1)
+            return np.asarray([int(x) for x in recseq])
---
+++ ssvm/src/inference_lv.pyx
@@ -0,0 +1,169 @@
+import numpy as np
+import heapq as hq
+import sys
+cimport numpy as np
+
+"""
+Inference using the List Viterbi algorithm, which sequentially find the (k+1)-th best path/walk given the 1st, 2nd, ..., k-th best paths/walks. 
+Implementation is adapted from references:
+- Sequentially finding the N-Best List in Hidden Markov Models, Dennis Nilsson and Jacob Goldberger, IJCAI 2001.
+- A tutorial on hidden Markov models and selected applications in speech recognition, L.R. Rabiner, Proceedings of the IEEE, 1989.
+"""
+
+cdef class HeapItem:  # an item in heapq (min-heap)
+    cdef readonly float priority
+    cdef readonly object task, string
+    
+    def __init__(self, float priority, task):
+        self.priority = priority
+        self.task = task
+        self.string = str(priority) + ': ' + str(task)
+        
+    #def __lt__(self, other):
+    #    return self.priority < other.priority
+    
+    def __richcmp__(self, other, int op):
+        if op == 2: # ==
+            return self.priority == other.priority
+        elif op == 3: # !=
+            return self.priority != other.priority
+        elif op == 0: # <
+            return self.priority < other.priority
+        elif op == 1: # <=
+            return self.priority <= other.priority
+        elif op == 4: # >
+            return self.priority > other.priority
+        elif op == 5: # >=
+            return self.priority >= other.priority
+        else:
+            assert False
+            
+    def __repr__(self):
+        return self.string
+    
+    def __str__(self):
+        return self.string
+
+
+cpdef do_inference_list_viterbi(int ps, int L, int M,
+        np.ndarray[dtype=np.float64_t, ndim=2] unary_params, 
+        np.ndarray[dtype=np.float64_t, ndim=3] pw_params, 
+        np.ndarray[dtype=np.float64_t, ndim=2] unary_features, 
+        np.ndarray[dtype=np.float64_t, ndim=3] pw_features, 
+        y_true=None, y_true_list=None):
+    """
+    Inference using the list Viterbi algorithm, could be:
+    - Train/prediction inference for single-label SSVM
+    - Train/prediction inference for multi-label SSVM
+    """
+    assert(L > 1)
+    assert(M >= L)
+    assert(ps >= 0)
+    assert(ps < M)
+    if y_true is not None: assert(y_true_list is not None and type(y_true_list) == list)
+    
+    cdef int pi, pj, t, k, pk, parix, partition_index, partition_index_start, k_partition_index
+    cdef long nIter, maxIter = long(1e6)
+    cdef float loss, priority
+    
+    Cu = np.zeros(M, dtype=np.float)      # unary_param[p] x unary_features[p]
+    Cp = np.zeros((M, M), dtype=np.float) # pw_param[pi, pj] x pw_features[pi, pj]
+    
+    # a intermediate POI should NOT be the start POI, NO self-loops
+    for pi in range(M):
+        Cu[pi] = np.dot(unary_params[pi, :], unary_features[pi, :]) # if pi != ps else -np.inf
+        for pj in range(M):
+            Cp[pi, pj] = -np.inf if (pj == ps or pi == pj) else np.dot(pw_params[pi, pj, :], pw_features[pi, pj, :])
+            
+    # forward-backward procedure: adapted from the Rabiner paper
+    Alpha = np.zeros((L, M), dtype=np.float)  # alpha_t(p_i)
+    Beta  = np.zeros((L, M), dtype=np.float)  # beta_t(p_i)
+    
+    for pj in range(M): Alpha[1, pj] = Cp[ps, pj] + Cu[pj] + (0 if y_true is None else float(pj != y_true[1]))
+    for t in range(2, L):
+        for pj in range(M): # ps~~pi--pj
+            loss = 0 if y_true is None else float(pj != y_true[t])  # pi varies, pj fixed
+            Alpha[t, pj] = loss + np.max([Alpha[t-1, pi] + Cp[pi, pj] + Cu[pj] for pi in range(M)])
+    
+    for pi in range(M): Beta[L-1, pi] = 0 if y_true is None else float(pi != y_true[L-1])
+    for t in range(L-1, 1, -1):
+        for pi in range(M): # ps~~pi--pj
+            loss = 0. if y_true is None else float(pi != y_true[t-1])  # pi fixed, pj varies
+            Beta[t-1, pi] = loss + np.max([Cp[pi, pj] + Cu[pj] + Beta[t, pj] for pj in range(M)])
+    Beta[0, ps] = np.max([Cp[ps, pj] + Cu[pj] + Beta[1, pj] for pj in range(M)])
+    
+    Fp = np.zeros((L-1, M, M), dtype=np.float)  # f_{t, t+1}(p, p')
+    
+    for t in range(L-1):
+        for pi in range(M):
+            for pj in range(M):
+                Fp[t, pi, pj] = Alpha[t, pi] + Cp[pi, pj] + Cu[pj] + Beta[t+1, pj]
+                
+    # identify the best path/walk: adapted from the IJCAI01 paper
+    y_best = np.ones(L, dtype=np.int) * (-1)
+    y_best[0] = ps
+    y_best[1] = np.argmax(Fp[0, ps, :])  # the start POI is specified
+    for t in range(2, L): 
+        y_best[t] = np.argmax(Fp[t-1, y_best[t-1], :])
+    
+    Q = []  # priority queue (min-heap)
+    with np.errstate(invalid='raise'):  # deal with overflow
+        try: nIter = np.power(M, L-1) - np.prod([M-kx for kx in range(1,L)]) + 1 + \
+                     (0 if y_true is None else len(y_true_list))
+        except: nIter = maxIter
+    nIter = np.min([nIter, maxIter])
+        
+    # heap item for the best path/walk
+    priority = -np.max(Alpha[L-1, :])
+    partition_index = -1
+    exclude_set = set()  # -1 * score as priority
+    hq.heappush(Q, HeapItem(priority, (y_best, partition_index, exclude_set)))
+    
+    k = 0; y_last = None
+    while len(Q) > 0 and k < nIter:
+        hitem = hq.heappop(Q)
+        k_priority, (k_best, k_partition_index, k_exclude_set) = hitem.priority, hitem.task
+        k += 1; y_last = k_best
+        
+        if len(set(k_best)) == L:
+            if y_true is None: return k_best
+            else: # return k_best if it is NOT one of the ground truth labels
+                if not np.any([np.all(np.asarray(k_best) == np.asarray(yj)) for yj in y_true_list]):
+                    return k_best
+
+        # identify the (k+1)-th best path/walk given the 1st, 2nd, ..., k-th best: adapted from the IJCAI01 paper
+        partition_index_start = 1
+        if k_partition_index > 0:
+            assert(k_partition_index < L)
+            partition_index_start = k_partition_index
+            
+        for parix in range(partition_index_start, L):    
+            new_exclude_set = set({k_best[parix]})
+            if parix == partition_index_start:
+                new_exclude_set = new_exclude_set | k_exclude_set
+            
+            new_best = np.zeros(L, dtype=np.int) * (-1)
+            for pk in range(parix):
+                new_best[pk] = k_best[pk]
+            
+            candidate_points = [p for p in range(M) if p not in new_exclude_set]
+            if len(candidate_points) == 0: continue
+            candidate_maxix = np.argmax([Fp[parix-1, k_best[parix-1], p] for p in candidate_points])
+            new_best[parix] = candidate_points[candidate_maxix]
+            
+            for pk in range(parix+1, L):
+                new_best[pk] = np.argmax([Fp[pk-1, new_best[pk-1], p] for p in range(M)])
+            
+            new_priority = Fp[parix-1, k_best[parix-1], new_best[parix]]
+            
+            if k_partition_index > 0:
+                new_priority += (-k_priority) - Fp[parix-1, k_best[parix-1], k_best[parix]]
+            new_priority *= -1.0   # NOTE: -np.inf - np.inf + np.inf = nan
+            
+            hq.heappush(Q, HeapItem(new_priority, (new_best, parix, new_exclude_set)))
+            
+    if k >= nIter: 
+        sys.stderr.write('WARN: reaching max number of iterations, NO optimal solution found, return the last one.\n')
+    if len(Q) == 0:
+        sys.stderr.write('WARN: empty queue, return the last one\n')
+    return y_last
