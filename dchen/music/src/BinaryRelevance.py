f2de1a09769e4f4bdd7cd326361c6b88196596be
Dawei Chen <dawei.chen@data61.csiro.au>
2017-12-07 20:08:22 +1100

    fix flake8 complaints

--- dchen/music/src/BinaryRelevance.py
+++ dchen/music/src/BinaryRelevance.py
@@ -7,24 +7,24 @@ class BinaryRelevance(BaseEstimator):
     """
         Independent logistic regression based on OneVsRestClassifier wrapper.
     """
-    
+
     def __init__(self, C=1):
         assert C > 0
         self.C = C
         self.trained = False
-        
+
     def fit(self, X_train, Y_train):
         assert X_train.shape[0] == Y_train.shape[0]
         # don't make two changes at the same time
-        #self.estimator = OneVsRestClassifier(LogisticRegression(class_weight='balanced', C=self.C))
+        # self.estimator = OneVsRestClassifier(LogisticRegression(class_weight='balanced', C=self.C))
         self.estimator = OneVsRestClassifier(LogisticRegression(C=self.C))
         self.estimator.fit(X_train, Y_train)
         self.trained = True
-        
+
     def decision_function(self, X_test):
         assert self.trained is True
         return self.estimator.decision_function(X_test)
-    
+
     def predict(self, X_test, binarise=False):
         preds = self.decision_function(X_test)
         return preds >= 0.5 if binarise is True else preds
