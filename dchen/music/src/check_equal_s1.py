547e9b0e73ad319bc252afac62a67a212f287e81
Dawei Chen <dawei.chen@data61.csiro.au>
2018-02-16 20:05:15 +1100

    make dataset split reproducible

---
+++ dchen/music/src/check_equal_s1.py
@@ -0,0 +1,43 @@
+import gzip
+import os, sys
+import pickle as pkl
+import numpy as np
+from scipy.sparse import csr_matrix
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage:', sys.argv[0], 'DIR1 DIR2')
+        sys.exit(0)
+
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+
+print('checking songs ...')
+
+songs1 = pkl.load(gzip.open(os.path.join(dir1, 'songs_train_dev_test_s1.pkl.gz'), 'rb'))
+songs2 = pkl.load(gzip.open(os.path.join(dir2, 'songs_train_dev_test_s1.pkl.gz'), 'rb'))
+for key in songs1:
+    assert np.all(np.array(songs1[key]) == np.array(songs2[key]))
+
+print('checking playlists ...')
+
+pl1 = pkl.load(gzip.open(os.path.join(dir1, 'playlists_s1.pkl.gz'), 'rb'))
+pl2 = pkl.load(gzip.open(os.path.join(dir2, 'playlists_s1.pkl.gz'), 'rb'))
+assert np.all(np.array(pl1) == np.array(pl2))
+
+
+print('checking features ...')
+
+for fname in ['X_train', 'X_dev', 'X_test']:
+    x1 = pkl.load(gzip.open(os.path.join(dir1, fname + '.pkl.gz'), 'rb'))
+    x2 = pkl.load(gzip.open(os.path.join(dir2, fname + '.pkl.gz'), 'rb'))
+    assert np.all(np.isclose(x1, x2))
+
+print('checking labels (sparse boolean matrices) ...')
+
+for fname in ['Y_train', 'Y_dev', 'Y_test']:
+    y1 = pkl.load(gzip.open(os.path.join(dir1, fname + '.pkl.gz'), 'rb'))
+    y2 = pkl.load(gzip.open(os.path.join(dir2, fname + '.pkl.gz'), 'rb'))
+    assert np.all(np.equal(y1.indices, y2.indices))
+
