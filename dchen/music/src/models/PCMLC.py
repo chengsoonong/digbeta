ab6103024a258c3fdba7b8ce70438e2876e9c36a
Dawei Chen <dawei.chen@data61.csiro.au>
2018-02-16 18:15:50 +1100

    Consume positive-only labels before unobserved in mini-batch GD

--- dchen/music/src/models/PCMLC.py
+++ dchen/music/src/models/PCMLC.py
@@ -1,45 +1,51 @@
-import os
 import sys
 import time
 import numpy as np
 from scipy.optimize import minimize
 from scipy.special import logsumexp
 from sklearn.base import BaseEstimator
-from scipy.sparse import issparse, csr_matrix
-import pickle as pkl
+from scipy.sparse import issparse
 
 
-def obj_pclassification(w, X, Y_pos, Y_neg, p, C1=1, C2=1, C3=1, weighting='labels', similarMat=None):
+def obj_pclassification(w, X, Y, p=1, C1=1, C2=1, C3=1, loss_type='example', PU=None, similarMat=None):
     """
         Objective with L2 regularisation and p-classification loss
 
         Input:
-            - w: current weight vector, flattened L x D + 1 (bias)
+            - w: current weight vector, flattened K x D + 1 (bias)
             - X: feature matrix, N x D
-            - Y: label matrix,   N x L
+            - Y: label matrix,   N x K
             - p: constant for p-classification push loss
             - C1-C3: regularisation constants
-            - weighting: weight the exponential surrogate by the #positive or #negative labels or samples,
-                  valid assignment: None, 'samples', 'labels', 'both'
+            - loss_type: weighting the exponential surrogate by the #positive or #negative labels or samples,
+                  valid assignment: None, 'example', 'label', 'both'
                   - None: do not weight
-                  - 'samples': weight by the #positive or #negative samples per label (weighting vertically)
-                  - 'labels': weight by the #positive or #negative labels per example (weighting horizontally)
-                  - 'both': equivalent to turn on both 'samples' and 'labels'
-            - PU: positive only matrix, PU.shape = Y.shape
-            - similarMat: square symmetric matrix, require the parameters of label_i and label_j should be similar
-                          (by regularising their difference) if entry (i,j) is 1.
-                          This is the adjacent matrix of playlists (nodes), and playlists of the same user form a clique.
+                  - 'example': weighting each example, by the #positive or #negative labels per example
+                  - 'label': weighting each label, by the #positive or #negative samples per label
+                  - 'both': equivalent to turn on both 'example' and 'label'
+            - PU: positive only matrix (with additional positive labels), PU.shape[0] = Y.shape[0]
+            - similarMat: the adjacent matrix of playlists (nodes), and playlists of the same user form a clique.
+                          To require require the parameters of label_i and label_j be similar by regularising 
+                          the diff if entry (i,j) is 1 (i.e. belong to the same user).
     """
     N, D = X.shape
-    K = Y_pos.shape[1]
-    assert Y_pos.shape == Y_neg.shape
+    K = Y.shape[1]
+    Yp = Y
+    Yn = 1 - Y
+
+    if PU is not None:
+        assert PU.shape[0] == Y.shape[0]
+        K += PU.shape[1]
+        Yp = np.hstack([Yp, PU])
+        Yn = np.hstack([Yn, np.zeros_like(PU, dtype=Yn.dtype)])
+
     assert w.shape[0] == K * D + 1
     assert p >= 1
     assert C1 > 0
     assert C2 > 0
     assert C3 > 0
-    assert weighting in [None, 'samples', 'labels', 'both'], \
-        'Valid assignment for "weighting" are: None, "samples", "labels", "both".'
+    assert loss_type in [None, 'example', 'label', 'both'], \
+        'Valid assignment for "loss_type" are: None, "example", "label", "both".'
     if similarMat is not None:
         # assert similarMat.shape == (K, K)
         # assert np.isclose(np.sum(1. * similarMat - 1. * similarMat.T), 0)  # trust the input
@@ -50,29 +56,18 @@ def obj_pclassification(w, X, Y_pos, Y_neg, p, C1=1, C2=1, C3=1, weighting='labe
         if nrows > K:
             similarMat = similarMat[:K, :K]
 
-    Yp = Y_pos.astype(np.int)
-    Yn = Y_neg.astype(np.int)
-
-    W = w[1:].reshape(K, D)  # reshape weight matrix
-    b = w[0]                 # bias
-
-    T1 = np.dot(X, W.T) + b  # N by K
-    # T2 = np.multiply(-Yp + p * Yn, T1)
-    T2 = np.multiply(Yp, -T1)
-    T3 = np.multiply(Yn, p * T1)
-
-    def _cost_grad(weight):
-        if weight is None:
+    def _cost_grad(loss_type):
+        if loss_type is None:
             Tp = 0
             Tn = np.log(p) * Yn
             P = Yp
             Q = Yn
             num = 1
             return Tp, Tn, P, Q, num
-        if weight == 'samples':
+        if loss_type == 'label':
             ax = 0
             shape = (1, K)
-        elif weight == 'labels':
+        elif loss_type == 'example':
             ax = 1
             shape = (N, 1)
         numPosAll = np.sum(Yp, axis=ax)
@@ -92,29 +87,40 @@ def obj_pclassification(w, X, Y_pos, Y_neg, p, C1=1, C2=1, C3=1, weighting='labe
         num = np.prod(shape)
         return Tp, Tn, P, Q, num
 
-    if weighting == 'both':
-        Tp1, Tn1, P1, Q1, num1 = _cost_grad(weight='samples')
-        Tp2, Tn2, P2, Q2, num2 = _cost_grad(weight='labels')
+    W = w[1:].reshape(K, D)  # reshape weight matrix
+    b = w[0]                 # bias
+    T1 = np.dot(X, W.T) + b  # N by K
+    T2 = np.multiply(Yp, -T1)
+    T3 = np.multiply(Yn, p * T1)
+
+    # No L2 regularisation for weights corresponding to PU
+    W1 = W.copy()
+    #if PU is not None:
+    #    W1[-PU.shape[1]:, :] = 0
+
+    if loss_type == 'both':
+        Tp1, Tn1, P1, Q1, num1 = _cost_grad(loss_type='label')
+        Tp2, Tn2, P2, Q2, num2 = _cost_grad(loss_type='example')
         T5 = T2 + T3 - Tp1 - Tn1 - np.log(num1)
         T6 = T2 + T3 - Tp2 - Tn2 - np.log(num2) + np.log(C2)
         cost = logsumexp(np.concatenate([T5.ravel(), T6.ravel()], axis=-1))
-        J = np.dot(W.ravel(), W.ravel()) * 0.5 / C1 + cost
         T7 = np.exp(T2 - cost)
         T8 = np.exp(T3 - cost)
         T11 = -np.multiply(P1, T7) + np.multiply(Q1, T8)
         T12 = -np.multiply(P2, T7) + np.multiply(Q2, T8)
-        dW = W / C1 + np.dot(T11.T, X) / num1 + C2 * np.dot(T12.T, X) / num2
+        dW = W1 / C1 + np.dot(T11.T, X) / num1 + C2 * np.dot(T12.T, X) / num2
         db = np.sum(T11) / num1 + C2 * np.sum(T12) / num2
     else:
-        Tp, Tn, P, Q, num = _cost_grad(weighting)
+        Tp, Tn, P, Q, num = _cost_grad(loss_type=loss_type)
         cost = logsumexp((T2 + T3 - Tp - Tn).ravel()) - np.log(num)
         # cost = logsumexp((T2 + T3 - Tp - Tn - np.log(num)).ravel())
-        J = np.dot(W.ravel(), W.ravel()) * 0.5 / C1 + cost
         T5 = -np.multiply(P, np.exp(T2 - cost)) + np.multiply(Q, np.exp(T3 - cost))
-        dW = W / C1 + np.dot(T5.T, X) / num
+        dW = W1 / C1 + np.dot(T5.T, X) / num
         db = np.sum(T5) / num
-    print('\n%.6f' % cost)
 
+    J = np.dot(W1.ravel(), W1.ravel()) * 0.5 / C1 + cost
+
+    # Multi-task regularisation, including regularise weights corresponding to PU
     if similarMat is not None:
         if issparse(similarMat):
             sumVec = similarMat.sum(axis=1)  # K by 1: #playlists_of_u - 1
@@ -150,7 +156,7 @@ def obj_pclassification(w, X, Y_pos, Y_neg, p, C1=1, C2=1, C3=1, weighting='labe
 class PCMLC(BaseEstimator):
     """All methods are necessary for a scikit-learn estimator"""
 
-    def __init__(self, p=1, C1=1, C2=1, C3=1, weighting=True, similarMat=None):
+    def __init__(self, p=1, C1=1, C2=1, C3=1, loss_type='example'):
         """Initialisation"""
 
         assert C1 > 0
@@ -161,42 +167,41 @@ class PCMLC(BaseEstimator):
         self.C2 = C2
         self.C3 = C3
         self.p = p
-        self.weighting = weighting
-        self.similarMat = similarMat
+        self.loss_type = loss_type 
         self.obj_func = obj_pclassification
         self.cost = []
         self.trained = False
 
-    def fit(self, X_train, Y_train, PUMat=None):
+    def fit(self, X_train, Y_train, PUMat=None, similarMat=None):
         """
             Model fitting by optimising the objective
             - PUMat: indicator matrix for additional labels with only positive observations.
                      If it is sparse, all missing entries are considered unobserved instead of negative;
                      if it is dense, it contains only 1 and NaN entries, and NaN entries are unobserved.
         """
-        opt_method = 'L-BFGS-B'  # 'BFGS' #'Newton-CG'
-        options = {'disp': 1, 'maxiter': 10**5, 'maxfun': 10**5}  # 'eps': 1e-5}  # , 'iprint': 99}
-        sys.stdout.write('\nC: %g, %g, %g, p: %g, weighting: %s' %
-                         (self.C1, self.C2, self.C3, self.p, self.weighting))
+        sys.stdout.write('\nC: %g, %g, %g, p: %g, loss_type: %s' %
+                         (self.C1, self.C2, self.C3, self.p, self.loss_type))
         sys.stdout.flush()
-        
-        Y_pos = Y_train
-        Y_neg = 1 - Y_train
-        if PUMat is not None:
-            assert PUMat.shape[0] == Y_train.shape[0]
-            if issparse(PUMat):
-                PUMat = PUMat.toarray()
-            Y_pos = np.hstack([Y_pos, PUMat])
-            Y_neg = np.hstack([Y_neg, np.zeros_like(PUMat, dtype=np.bool)])
 
         N, D = X_train.shape
-        K = Y_pos.shape[1]
+        K = Y_train.shape[1]
+
+        if PUMat is not None:
+            assert PUMat.shape[0] == Y_train.shape[0]
+            K += PUMat.shape[1]
+            if issparse(PUMat):
+                PUMat = PUMat.toarray()
+
         if PUMat is None:
             w0 = np.zeros(K * D + 1)
         else:
-            w0 = np.concatenate([np.zeros(Y_train.shape[1] * D + 1), 0.001 * np.random.randn(PUMat.shape[1] * D)], axis=-1)
+            K1 = PUMat.shape[1]
+            w0 = np.r_[np.zeros((K-K1) * D + 1), 0.001 * np.random.randn(K1 * D)]
+
+        opt_method = 'L-BFGS-B'  # 'BFGS' #'Newton-CG'
+        options = {'disp': 1, 'maxiter': 10**5, 'maxfun': 10**5}  # 'eps': 1e-5}  # , 'iprint': 99}
         opt = minimize(self.obj_func, w0,
-                       args=(X_train, Y_pos, Y_neg, self.p, self.C1, self.C2, self.C3, self.weighting, self.similarMat),
+                       args=(X_train, Y_train, self.p, self.C1, self.C2, self.C3, self.loss_type, PUMat, similarMat),
                        method=opt_method, jac=True, options=options)
         if opt.success is True:
             self.b = opt.x[0]
@@ -207,7 +212,8 @@ class PCMLC(BaseEstimator):
             print(opt.items())
             self.trained = False
 
-    def fit_minibatch(self, X_train, Y_train, PUMat=None, w0=None, learning_rate=0.001, batch_size=256, n_epochs=10, verbose=0):
+    def fit_minibatch(self, X_train, Y_train, PUMat=None, similarMat=None, w0=None,
+                      learning_rate=0.001, batch_size=256, n_epochs=10, verbose=0):
         """
             Model fitting by mini-batch Gradient Descent
             - PUMat: indicator matrix for additional labels with only positive observations.
@@ -221,18 +227,19 @@ class PCMLC(BaseEstimator):
             assert not np.logical_xor(issparse(PUMat), issparse(Y_train))
             K += PUMat.shape[1]
 
-        fnpy = ('mlr' if PUMat is None else 'pla') + ('-N-' if self.similarMat is None else '-Y-') + \
-               '%s-%g-%g-%g-%g-latest.npy' % (self.weighting, self.C1, self.C2, self.C3, self.p)
+        fnpy = ('mlr' if PUMat is None else 'pla') + ('-N-' if similarMat is None else '-Y-') + \
+            '%s-%g-%g-%g-%g-latest.npy' % (self.loss_type, self.C1, self.C2, self.C3, self.p)
 
         if w0 is None:
             try:
                 w = np.load(fnpy, allow_pickle=False)
-                print('restore from %s' % fnpy)
-            except:
+                print('Restore from %s' % fnpy)
+            except (IOError, ValueError):
                 if PUMat is None:
                     w = np.zeros(K * D + 1)
                 else:
-                    w = np.concatenate([np.zeros(Y_train.shape[1] * D + 1), 0.001 * np.random.randn(PUMat.shape[1] * D)], axis=-1)
+                    K1 = PUMat.shape[1]
+                    w = np.r_[np.zeros((K-K1) * D + 1), 0.001 * np.random.randn(K1 * D)]
         else:
             assert w0.shape[0] == K * D + 1
             w = w0
@@ -245,37 +252,44 @@ class PCMLC(BaseEstimator):
         beta1_t = beta1
         beta2_t = beta2
         n_batches = int((N-1) / batch_size) + 1
-        
-        # np.random.seed(918273645)
+
+        pusum = np.asarray(PUMat.sum(axis=1)).reshape(-1)
+        prows = []
+        urows = []
+        for row in range(N):
+            if pusum[row] > 0:
+                prows.append(row)
+            else:
+                urows.append(row)
+
+        np.random.seed(91827365)
         for epoch in range(n_epochs):
             if verbose > 0:
                 print(time.strftime('%Y-%m-%d %H:%M:%S'))
-            indices = np.arange(N)
-            np.random.shuffle(indices)
+            # indices = np.arange(N)
+            # np.random.shuffle(indices)
+            indices = np.r_[np.random.permutation(prows), np.random.permutation(urows)]
 
             for nb in range(n_batches):
                 if verbose > 0:
-                    sys.stdout.write('\r %d / %d' % (nb+1, n_batches))
-                    sys.stdout.flush()
+                    sys.stdout.write('%d / %d' % (nb+1, n_batches))
                 ix_start = nb * batch_size
                 ix_end = min((nb+1) * batch_size, N)
                 ix = indices[ix_start:ix_end]
                 X = X_train[ix]
-                Y_pos = Y_train[ix]
+                Y = Y_train[ix]
+                if issparse(Y):
+                    Y = Y.toarray().astype(np.bool)
 
-                if issparse(Y_pos):
-                    Y_pos = Y_pos.toarray().astype(np.bool)
-                Y_neg = 1 - Y_pos
+                PU = None
                 if PUMat is not None:
                     PU = PUMat[ix]
                     if issparse(PU):
                         PU = PU.toarray().astype(np.bool)
-                    Y_pos = np.hstack([Y_pos, PU])
-                    Y_neg = np.hstack([Y_neg, np.zeros_like(PU, dtype=np.bool)])
 
-                J, dw = self.obj_func(w, X, Y_pos=Y_pos, Y_neg=Y_neg, p=self.p, C1=self.C1, C2=self.C2, C3=self.C3,
-                                     weighting=self.weighting,similarMat=self.similarMat)
-             
+                J, dw = self.obj_func(w=w, X=X, Y=Y, p=self.p, C1=self.C1, C2=self.C2, C3=self.C3,
+                                      loss_type=self.loss_type, PU=PU, similarMat=similarMat)
+
                 # https://www.tensorflow.org/api_docs/python/tf/train/AdamOptimizer
                 alpha_t = learning_rate * np.sqrt(1. - beta2_t) / (1. - beta1_t)
                 m_t = beta1 * m_t + (1. - beta1) * dw
@@ -285,12 +299,13 @@ class PCMLC(BaseEstimator):
                 beta2_t *= beta2
 
                 if np.isnan(J):
-                    print('J = NaN, training failed.')
+                    print('\nJ = NaN, training failed.')
                     return
 
                 self.cost.append(J)
                 if verbose > 0:
                     print(' | alpha: %.6f, |dw|: %.6f, objective: %.6f' % (alpha_t, np.sqrt(np.dot(dw, dw)), J))
+                    sys.stdout.flush()
 
             print('\nepoch: %d / %d' % (epoch+1, n_epochs))
             np.save(fnpy, w, allow_pickle=False)
@@ -316,4 +331,3 @@ class PCMLC(BaseEstimator):
     #
     # def get_params(self, deep = True):
     # def set_params(self, **params):
-
