1f8f701c822e996edaaa01e2d387dc4a092436cf
Dawei Chen <dawei.chen@data61.csiro.au>
2018-02-17 18:06:00 +1100

    script for pla on cluster

--- dchen/music/src/aotm2011_pla.py
+++ dchen/music/src/aotm2011_pla.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 import pickle as pkl
 
-if len(sys.argv) != 7:
-    print('Usage: python', sys.argv[0], 'WORK_DIR  C1  C2  C3  P  N_EPOCH')
+if len(sys.argv) != 8:
+    print('Usage: python', sys.argv[0], 'WORK_DIR  C1  C2  C3  P  N_EPOCH LOSS_TYPE(example/label/both)')
     sys.exit(0)
 else:
     work_dir = sys.argv[1]
@@ -15,32 +15,35 @@ else:
     C3 = float(sys.argv[4])
     p = float(sys.argv[5])
     n_epochs = int(sys.argv[6])
+    loss = sys.argv[7]
+
+assert loss in ['example', 'label', 'both']
 
 data_dir = os.path.join(work_dir, 'data')
 src_dir = os.path.join(work_dir, 'src')
 sys.path.append(src_dir)
 
-from PCMLC import PCMLC as PClassificationMLC
+from models import PCMLC
 
 pkl_data_dir = os.path.join(data_dir, 'aotm-2011/setting2')
 fxtrain = os.path.join(pkl_data_dir, 'X_train.pkl.gz')
 fytrain = os.path.join(pkl_data_dir, 'Y_train.pkl.gz')
 fydev = os.path.join(pkl_data_dir, 'PU_dev.pkl.gz')
-fadjmat = os.path.join(pkl_data_dir, 'adjacency_mat2.pkl.gz')
+fcliques = os.path.join(pkl_data_dir, 'cliques_train_dev.pkl.gz')
 
 X_train = pkl.load(gzip.open(fxtrain, 'rb'))
 Y_train = pkl.load(gzip.open(fytrain, 'rb'))
 PU_dev = pkl.load(gzip.open(fydev, 'rb'))
-same_user_mat = pkl.load(gzip.open(fadjmat, 'rb'))
-userwiseReg = False
+cliques = pkl.load(gzip.open(fcliques, 'rb'))
+#cliques = None
 
 print('C: %g, %g, %g, p: %g' % (C1, C2, C3, p))
 
 print(time.strftime('%Y-%m-%d %H:%M:%S'))
-clf = PClassificationMLC(C1=C1, C2=C2, C3=C3, p=p,  weighting='both', similarMat=same_user_mat, userwiseReg=userwiseReg)
-clf.fit_minibatch(X_train, Y_train, PUMat=PU_dev, batch_size=1024, n_epochs=n_epochs, learning_rate=0.1, verbose=1)
+clf = PCMLC(C1=1, C2=1, C3=1, p=1,  loss_type=loss)
+clf.fit_minibatch(X_train, Y_train, PUMat=PU_dev, user_playlist_indices=cliques, batch_size=512, n_epochs=n_epochs, verbose=1)
 
 if clf.trained is True:
-    fmodel = os.path.join(pkl_data_dir, 'pla-aotm2011-C-%g-%g-%g-p-%g.pkl' % (C1, C2, C3, p))
-    pkl.dump(clf, open(fmodel, 'wb'))
+    fmodel = os.path.join(pkl_data_dir, 'pla-%s-%g-%g-%g-%g.pkl.gz' % (loss, C1, C2, C3, p))
+    pkl.dump(clf, gzip.open(fmodel, 'wb'))
 
