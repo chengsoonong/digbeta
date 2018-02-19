c6979d37b1ae3603f5fedb8f99995affb359d594
Dawei Chen <dawei.chen@data61.csiro.au>
2018-02-19 13:47:27 +1100

    add evaluation

--- dchen/music/src/aotm2011_pla.py
+++ dchen/music/src/aotm2011_pla.py
@@ -29,11 +29,13 @@ data_dir = os.path.join(work_dir, 'data')
 pkl_data_dir = os.path.join(data_dir, 'aotm-2011/setting2')
 fxtrain = os.path.join(pkl_data_dir, 'X_train.pkl.gz')
 fytrain = os.path.join(pkl_data_dir, 'Y_train.pkl.gz')
+fytrndev = os.path.join(pkl_data_dir, 'Y_train_dev.pkl.gz')
 fydev = os.path.join(pkl_data_dir, 'PU_dev.pkl.gz')
 fcliques = os.path.join(pkl_data_dir, 'cliques_train_dev.pkl.gz')
 
 X_train = pkl.load(gzip.open(fxtrain, 'rb'))
 Y_train = pkl.load(gzip.open(fytrain, 'rb'))
+Y_train_dev = pkl.load(gzip.open(fytrndev, 'rb'))
 PU_dev = pkl.load(gzip.open(fydev, 'rb'))
 if multitask == 'Y':
     cliques = pkl.load(gzip.open(fcliques, 'rb'))
@@ -49,4 +51,19 @@ clf.fit_minibatch_pla(X_train, Y_train, PUMat=PU_dev, user_playlist_indices=cliq
 if clf.trained is True:
     fmodel = os.path.join(pkl_data_dir, 'pla-%s-%g-%g-%g-%g.pkl.gz' % (loss, C1, C2, C3, p))
     pkl.dump(clf, gzip.open(fmodel, 'wb'))
+    Y_dev = Y_train_dev[:, -PU_dev.shape[1]:]
+    offset = Y_train_dev.shape[1] - PU_dev.shape[1]
+    W = clf.W
+    b = clf.b
+    aucs = []
+    for j in range(Y_dev.shape[1]):
+        y1 = Y_dev[:, j].toarray().reshape(-1)
+        y2 = PU_dev[:, j].toarray().reshape(-1)
+        indices = np.where(0 == y2)[0]
+        y_true = y1[indices]
+        assert y_true.sum() + PU_dev[:, j].sum() == Y_dev[:, j].sum()
+        wj = W[j + offset, :].reshape(-1)
+        y_pred = (np.dot(X_train, wj) + b)[indices]
+        aucs.append(roc_auc_score(y_true, y_pred))
+    print('\n%.5f, %d / %d' % (np.mean(aucs), len(aucs), Y_dev.shape[1]))
 
