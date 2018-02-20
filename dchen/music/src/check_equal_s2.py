e9239055091ff70181063024484e15addd45e501
Dawei Chen <dawei.chen@data61.csiro.au>
2018-02-20 19:54:13 +1100

    check more

--- dchen/music/src/check_equal_s2.py
+++ dchen/music/src/check_equal_s2.py
@@ -14,6 +14,13 @@ dir1 = sys.argv[1]
 dir2 = sys.argv[2]
 
 
+print('checking songs ...')
+
+songs1 = pkl.load(gzip.open(os.path.join(dir1, 'all_songs.pkl.gz'), 'rb'))
+songs2 = pkl.load(gzip.open(os.path.join(dir2, 'all_songs.pkl.gz'), 'rb'))
+assert np.all(np.array(songs1) == np.array(songs2))
+
+
 print('checking playlists ...')
 
 pl1 = pkl.load(gzip.open(os.path.join(dir1, 'playlists_train_dev_test_s2.pkl.gz'), 'rb'))
@@ -34,4 +41,12 @@ for fname in ['Y', 'Y_train', 'Y_train_dev', 'PU_dev', 'PU_test']:
     y1 = pkl.load(gzip.open(os.path.join(dir1, fname + '.pkl.gz'), 'rb'))
     y2 = pkl.load(gzip.open(os.path.join(dir2, fname + '.pkl.gz'), 'rb'))
     assert np.all(np.equal(y1.indices, y2.indices))
+
+
+print('checking song popularities ...')
+
+pops1 = pkl.load(gzip.open(os.path.join(dir1, 'song2pop.pkl.gz'), 'rb'))
+pops2 = pkl.load(gzip.open(os.path.join(dir2, 'song2pop.pkl.gz'), 'rb'))
+for sid in pops1:
+    assert pops1[sid] == pops2[sid]
 
