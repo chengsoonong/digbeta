64c394a2aeb48292f4c491255631da75fb0cc863
Dawei Chen <dawei.chen@data61.csiro.au>
2018-04-05 23:25:26 +1000

    export MTC

--- dchen/music/src/models/__init__.py
+++ dchen/music/src/models/__init__.py
@@ -1,12 +1,12 @@
 from .BinaryRelevance import BinaryRelevance
-from .MLC import MLC, risk_pclassification, DataHelper, objective, \
-                 accumulate_risk_label, multitask_regulariser
+# from .MLC import MLC, risk_pclassification, DataHelper, objective, \
+#                 accumulate_risk_label, multitask_regulariser
 # from .PCMLC import PCMLC, risk_pclassification, DataHelper, objective, accumulate_risk_label, \
 #                    accumulate_risk_example, multitask_regulariser
+from .MTC import MTC, objective, risk_pclassification
 
 
 # __all__ = [s for s in dir() if not s.startswith('_')]
 # __all__ = [BinaryRelevance, PCMLC, risk_pclassification, DataHelper, objective,
 #           accumulate_risk_label, accumulate_risk_example, multitask_regulariser]
-__all__ = [BinaryRelevance, MLC, risk_pclassification, DataHelper, objective,
-           accumulate_risk_label, multitask_regulariser]
+__all__ = [BinaryRelevance, MTC, objective, risk_pclassification]
