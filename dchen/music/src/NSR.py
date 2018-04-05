9c1dd809f7942cbe3609150bfcc6eb9681340b5c
Dawei Chen <dawei.chen@data61.csiro.au>
2018-04-05 23:26:06 +1000

    script for new song recommendation

--- dchen/music/src/NSR.py
+++ dchen/music/src/NSR.py
@@ -4,24 +4,20 @@ import gzip
 import time
 import numpy as np
 import pickle as pkl
-from models import MLC
+from models import MTC
 
 
-if len(sys.argv) != 9:
+if len(sys.argv) != 6:
     print('Usage: python', sys.argv[0],
-          'WORK_DIR  DATASET  C1  C3  P  BATCH_SIZE  MT_REG(Y/N)  TRAIN_DEV(Y/N)')
+          'WORK_DIR  DATASET  C  P  TRAIN_DEV(Y/N)')
     sys.exit(0)
 else:
     work_dir = sys.argv[1]
     dataset = sys.argv[2]
-    C1 = float(sys.argv[3])
-    C3 = float(sys.argv[4])
-    p = float(sys.argv[5])
-    bs = int(sys.argv[6])
-    multitask = sys.argv[7]
-    trndev = sys.argv[8]
+    C = float(sys.argv[3])
+    p = float(sys.argv[4])
+    trndev = sys.argv[5]
 
-assert multitask in ['Y', 'N']
 assert trndev in ['Y', 'N']
 
 data_dir = os.path.join(work_dir, 'data/%s/setting1' % dataset)
@@ -31,16 +27,17 @@ if trndev == 'N':
     fytrain = os.path.join(data_dir, 'Y_train.pkl.gz')
     fxdev = os.path.join(data_dir, 'X_dev.pkl.gz')
     fydev = os.path.join(data_dir, 'Y_dev.pkl.gz')
-    fprefix = 'mlr-%s-%g-%g-%g' % (multitask, C1, C3, p)
+    fcliques = os.path.join(data_dir, 'cliques_train.pkl.gz')
+    fprefix = 'nsr-%g-%g' % (C, p)
 else:
     assert trndev == 'Y'
     fxtrain = os.path.join(data_dir, 'X_train_dev.pkl.gz')
     fytrain = os.path.join(data_dir, 'Y_train_dev.pkl.gz')
     fxdev = os.path.join(data_dir, 'X_test.pkl.gz')
     fydev = os.path.join(data_dir, 'Y_test.pkl.gz')
-    fprefix = 'trndev-mlr-%s-%g-%g-%g' % (multitask, C1, C3, p)
+    fcliques = os.path.join(data_dir, 'cliques_trndev.pkl.gz')
+    fprefix = 'trndev-nsr-%g-%g' % (C, p)
 
-fcliques = os.path.join(data_dir, 'cliques_all.pkl.gz')
 fmodel = os.path.join(data_dir, '%s.pkl.gz' % fprefix)
 fnpy = os.path.join(data_dir, '%s.npy' % fprefix)
 
@@ -48,13 +45,9 @@ X_train = pkl.load(gzip.open(fxtrain, 'rb'))
 Y_train = pkl.load(gzip.open(fytrain, 'rb'))
 X_dev = pkl.load(gzip.open(fxdev, 'rb'))
 Y_dev = pkl.load(gzip.open(fydev, 'rb'))
+cliques = pkl.load(gzip.open(fcliques, 'rb'))
 
-if multitask == 'Y':
-    cliques = pkl.load(gzip.open(fcliques, 'rb'))
-else:
-    cliques = None
-
-print('C: %g, %g, p: %g' % (C1, C3, p))
+print('C: %g, p: %g' % (C, p))
 print(X_train.shape, Y_train.shape)
 print(time.strftime('%Y-%m-%d %H:%M:%S'))
 
@@ -63,20 +56,20 @@ if os.path.exists(fmodel):
     clf = pkl.load(gzip.open(fmodel, 'rb'))   # for evaluation
 else:
     print('training ...')
-    clf = MLC(C1=C1, C3=C3, p=p)
-    clf.fit(X_train, Y_train, user_playlist_indices=cliques, batch_size=bs, njobs=3, verbose=2, fnpy=fnpy)
+    clf = MTC(X_train, Y_train, C=C, p=p, user_playlist_indices=cliques)
+    clf.fit(njobs=1, verbose=2, fnpy=fnpy)
 
 if clf.trained is True:
-    W = clf.W
-    b = clf.b
+    pkl.dump(clf, gzip.open(fmodel, 'wb'))
     rps = []
+    Y_pred = clf.predict(X_dev)
+    assert Y_dev.shape == Y_pred.shape
     for j in range(Y_dev.shape[1]):
         y_true = Y_dev[:, j].toarray().reshape(-1)
         npos = y_true.sum()
         if npos < 1:
             continue
-        wj = W[j, :].reshape(-1)
-        y_pred = np.dot(X_dev, wj) + b[0, j]
+        y_pred = Y_pred[:, j].reshape(-1)
         sortix = np.argsort(-y_pred)
         y_ = y_true[sortix]
         rps.append(np.mean(y_[:npos]))
